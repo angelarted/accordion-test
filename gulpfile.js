797fb37864f6c93c0f642a45dc2fb8589f71ebe4
Simona <simo.adria@gmail.com>
2018-12-05 15:19:06 +0100

    cambiato gulp che non faceva il watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,58 @@
-var gulp         = require('gulp');
-var sass         = require('gulp-sass');
-var babel        = require('gulp-babel');
-var cleanCSS     = require('gulp-clean-css');
-var uglify       = require('gulp-uglify');
-var browserSync  = require('browser-sync');
+var fs = require("fs");
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+let cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
 var autoprefixer = require('gulp-autoprefixer');
 
-// Config
-var config = {
-    srcCSS: 'src/**/*.scss',
-    distCSS: 'css',
-    srcJS: 'src/**/*.js',
-    distJS: 'js'
+var scriptSrc = ['./src/*.js'];
+var sassSrc = ['./src/*.scss'];
+var reload = browserSync.reload;
+
+
+function styles() {
+  return gulp.src(sassSrc)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['> 1%',
+            'last 3 versions',
+            'ie >= 10'],
+            cascade: false,
+            grid: true
+        }))       
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());//instead of reloading, injecting css
 }
 
-// Browser Sync
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: './'
+
+function scripts() {
+  return gulp.src(scriptSrc)
+    .pipe(gulp.dest('./js'));
+}
+
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            directory: true
+        }
     });
-});
 
-// Javascript
-gulp.task('js', function() {
-    return gulp.src(config.srcJS)
-        .pipe(babel({retainLines: true}))
-        .pipe(uglify())
-        .pipe(gulp.dest(config.distJS))
-        .pipe(browserSync.stream());
-});
+}
 
-// Sass
-gulp.task('sass', function() {
-    return gulp.src(config.srcCSS)
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(autoprefixer({cascade: false}))
-        .pipe(gulp.dest(config.distCSS))
-        .pipe(browserSync.stream())
-        .pipe(cleanCSS())
-        .pipe(gulp.dest(config.distCSS))
-        .pipe(browserSync.stream());
-});
+var build = gulp.series(gulp.parallel(styles, scripts));
 
-// Main task
-gulp.task('run', ['browser-sync', 'sass', 'js'], function() {
-    gulp.watch(config.srcCSS, ['sass']);
-    gulp.watch(config.srcJS, ['js']);
-    gulp.watch('*.html', browserSync.reload);
-});
 
-gulp.task('default', ['run']);
+function watch() {
+  gulp.watch(sassSrc, styles);
+  gulp.watch(scriptSrc, scripts).on('change', reload);
+  gulp.watch('./*.html' ).on('change', reload);
+}
+
+
+gulp.task('sass', styles );
+gulp.task('scripts', scripts );
+gulp.task('watch', gulp.parallel(watch, serve));
+gulp.task('build', build);
+gulp.task('serve', serve);
